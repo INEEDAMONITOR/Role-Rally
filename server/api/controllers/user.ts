c4f8ecddfe80fd2f2d69f6c08d721d519bcbce6c
siqi <yao.siq@northeastern.edu>
2024-02-03 20:23:29 -0500

    rm: useless try & catch

--- server/api/controllers/user.ts
+++ server/api/controllers/user.ts
@@ -1,30 +1,14 @@
 import { Request, Response } from "express";
 
 export const createUser = async (req: Request, res: Response) => {
-  try {
-    const sendbirdRes = await (await fetch(`https://api-${process.env.APPLICATION_ID}.sendbird.com/v3/users`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        "Api-Token": process.env.API_TOKEN as string,
-      },
-      body: JSON.stringify(req.body),
-    })).json();
+  const sendbirdRes = await (await fetch(`https://api-${process.env.APPLICATION_ID}.sendbird.com/v3/users`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      "Api-Token": process.env.API_TOKEN as string,
+    },
+    body: JSON.stringify(req.body),
+  })).json();
 
-    res.status(200).json(sendbirdRes);
-  } catch (e) {
-    if (e instanceof Error) {
-      res.status(403).json({
-        message: e.message,
-        error: true,
-      });
-    }
-
-    if (e instanceof String) {
-      res.status(403).json({
-        message: e,
-        error: true,
-      });
-    }
-  }
+  res.status(200).json(sendbirdRes);
 };
