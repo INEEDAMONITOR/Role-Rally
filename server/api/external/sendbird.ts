c0131a0a7459830c7034e7e72e1dc9256279c72c
siqi <yao.siq@northeastern.edu>
2024-02-03 21:03:32 -0500

    refactor: env

--- server/api/external/sendbird.ts
+++ server/api/external/sendbird.ts
@@ -1,1 +1,5 @@
-export const SENDBIRD_API_URL = `https://api-${process.env.APPLICATION_ID}.sendbird.com/v3`;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const SENDBIRD_API_URL = `https://api-${process.env.SENDBIRD_APP_ID}.sendbird.com/v3`;
