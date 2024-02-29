3211caa2aea313e7b136d0a827356e4d7df9611b
INEEDAMONITOR <joel.yuxi@gmail.com>
2024-02-28 19:50:30 -0500

    fix: remove roles id validate for creation limitation

--- app/api/_models/User.ts
+++ app/api/_models/User.ts
@@ -1,31 +1,30 @@
 import mongoose, { mongo } from "mongoose";
 import { Types } from "mongoose";
 
-export interface IUser extends mongoose.Document{
+export interface IUser {
   _id: Types.ObjectId;
   name: string;
   email: string;
   password: string;
-  profileId: Types.ObjectId,
-  rolesId: Types.ObjectId[],
+  profileId: Types.ObjectId;
+  rolesId: Types.ObjectId[];
 }
 
 const UserSchema = new mongoose.Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  profileId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Profile" },
+  profileId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Profile",
+  },
   rolesId: {
     type: [mongoose.Schema.Types.ObjectId],
     required: true,
-    validate: {
-      validator: function(v: Types.ObjectId[]) {
-        return Array.isArray(v) && v.length > 0; // Ensure that 'roles' is an array and has at least one element
-      },
-      message: "There must be at least one role."
-    },
-    ref: "Role"
-  }, 
+    ref: "Role",
+  },
 });
 
-export default mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
+export default mongoose.models.User ||
+  mongoose.model<IUser>("User", UserSchema);
