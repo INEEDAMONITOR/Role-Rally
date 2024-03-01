105d96b98bccf7deb082325dd2d72681c3edfda4
INEEDAMONITOR <joel.yuxi@gmail.com>
2024-03-01 18:31:52 -0500

    feat: impelement middleware handler for API

--- app/api/_middleware/user.ts
+++ app/api/_middleware/user.ts
@@ -1,6 +1,7 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { getUser } from "@/app/api/_services/user";
+import { CustomMiddleware } from "@/app/api/_middleware/handler";
 
 export const userAuthenticate = async (request: NextRequest) => {
   try {
@@ -19,5 +20,36 @@ export const userAuthenticate = async (request: NextRequest) => {
     return await getUser({ _id: jwtPayload._id }, ["email", "rolesId"]);
   } catch (e) {
     return null;
+  }
+};
+
+export const userAuthenticateWithId: CustomMiddleware<{
+  params: { id: string };
+}> = async (request, { params }, next) => {
+  try {
+    const token = request.cookies.get("roleRallyUserToken")?.value;
+
+    if (!token) {
+      return NextResponse.json(
+        { message: "No token provided" },
+        { status: 401 }
+      );
+    }
+
+    const jwtPayload = jwt.verify(token, process.env.JWT_SECRET as string);
+    if (typeof jwtPayload === "string") {
+      return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+    }
+
+    if (jwtPayload._id !== params.id) {
+      return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+    }
+    console.log(jwtPayload);
+
+    if (next != undefined) {
+      next();
+    }
+  } catch (e) {
+    return NextResponse.json({ message: "Internal error" }, { status: 500 });
   }
 };
